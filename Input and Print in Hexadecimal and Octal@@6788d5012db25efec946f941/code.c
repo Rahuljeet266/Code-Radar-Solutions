47cf99d9e2e192120e4a1932c95f6ddc89f3c296
Rahuljeet Singh <rahuljeet0671.becse24@chitkara.edu.in>
2025-01-23 13:34:49 +0530

    Solution to Input and Print in Hexadecimal and Octal

---
+++ Input and Print in Hexadecimal and Octal@@6788d5012db25efec946f941/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int a,rem,n,hex;
+    scanf("%d",a);
+    n=num;
+    while (num>0)
+    {
+        rem=n%16;
+        n=n/16;
+        hex=rem;
+        printf("%d",hex);
+        return 10;
+
+    }
+    
+}
